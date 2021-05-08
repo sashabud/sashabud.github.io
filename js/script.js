53250e1cd8b268b1de4f6da4b37e8de7b61d4b4e
sashabud <77093248+sashabud@users.noreply.github.com>
2021-05-08 18:23:48 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,194 @@
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+   /*  nav: false */
+    
+});
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
+
+const tabContent = document.querySelectorAll('.catalog__content'),
+    tabWrapper = document.querySelector('.catalog__tabs'),
+    tabs = document.querySelectorAll('.catalog__tab');
+
+function hideTabContent() {
+    tabContent.forEach(item => {
+        item.style.display = 'none';
+    });
+    tabs.forEach(item => {
+        item.classList.remove('catalog__tab_active');
+    });
+}
+
+function showTabContent(i = 0) {
+    tabContent[i].style.display = 'flex';
+    tabs[i].classList.add('catalog__tab_active');
+}
+
+hideTabContent();
+showTabContent();
+
+tabWrapper.addEventListener('click', (e) => {
+    const target = e.target;
+    console.log(target);
+    if (target && target.closest('.catalog__tab')) { // 1
+        console.log('2click');
+        tabs.forEach((item, i) => {
+            if (target == item || target.parentElement == item) {  // 2
+                hideTabContent();
+                showTabContent(i);
+            }
+        });
+    }
+});
+$(document).ready(function () {
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    //modal
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    };
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#up']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+});
+
+const scrolling = (upSelector) => {
+    const upElem = document.querySelector(upSelector);
+
+    window.addEventListener('scroll', () => {
+        if (document.documentElement.scrollTop > 1650) {
+            upElem.classList.add('animated', 'fadeIn');
+            upElem.classList.remove('fadeOut');
+        } else {
+            upElem.classList.add('fadeOut');
+            upElem.classList.remove('fadeIn');
+        }
+    });
+
+    // Scrolling with raf
+
+    let links = document.querySelectorAll('[href^="#"]'),
+        speed = 0.4;
+
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+
+            let widthTop = document.documentElement.scrollTop,
+                hash = this.hash,
+                toBlock = document.querySelector(hash).getBoundingClientRect().top,
+                start = null;
+
+            requestAnimationFrame(step);
+
+            function step(time) {
+                if (start === null) {
+                    start = time;
+                }
+
+                let progress = time - start,
+                    r = (toBlock < 0 ? Math.max(widthTop - progress / speed, widthTop + toBlock) : Math.min(widthTop + progress / speed, widthTop + toBlock));
+
+                document.documentElement.scrollTo(0, r);
+
+                if (r != widthTop + toBlock) {
+                    requestAnimationFrame(step);
+                } else {
+                    location.hash = hash;
+                }
+            }
+        });
+    });
+};
+
